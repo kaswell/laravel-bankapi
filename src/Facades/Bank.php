1fd54850cce80e8a87295a2edcc853266bf25159
Aliaksandr Kasatkin <aliaksandr.kasatkin@adshare.tv>
2021-06-04 15:18:01 +0300

    start project

--- src/Facades/Bank.php
+++ src/Facades/Bank.php
@@ -8,7 +8,7 @@ use Illuminate\Support\Facades\Facade;
  * Class Bank
  * @package Kaswell\Bank\Facades
  * @method static array getCurrencies()
- * @method static array getCurrencyById(int $cur_id = ZERO)
+ * @method static array getCurrencyById(int $cur_id = 0)
  * @method static array getCurrenciesRates($date = null, int $periodicity = 0)
  * @method static array getCurrencyRate($cur_id, $date = null, int $periodicity = 0, int $paramMode = 0)
  */
--- src/constants.php
+++ src/constants.php
@@ -1,9 +1,3 @@
 <?php
 
-const ZERO = 0;
 const EMPTY_ARRAY = [];
-
-const GET_METHOD = 'get';
-const POST_METHOD = 'post';
-const PUT_METHOD = 'put';
-const DELETE_METHOD = 'delete';
